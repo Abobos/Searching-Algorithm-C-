c04aa07c5dcb345efba8681d9910c11bed21967e
Blessing Makaraba <blessingmakaraba@gmail.com>
2020-11-08 20:47:13 +0100

    Implement Linear and Binary Search Algorithm for integers data type

---
+++ BinarySearch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearch
+{
+
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            var arrayToSearch = new List<int>() { 10, 14, 19, 26, 27, 31, 33, 35, 42, 44 };
+            var searcher = new Searcher(arrayToSearch);
+            Console.WriteLine(searcher.linearSearch(4));
+            Console.WriteLine(searcher.binarySearch(26));
+        }
+    }
+
+    
+}
---
+++ BinarySearch/Searcher.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearch
+{
+        public class Searcher
+        {
+            private readonly List<int> _proposedArray;
+
+            public Searcher(List<int> arrays)
+            {
+                _proposedArray = arrays;
+
+            }
+            public int linearSearch(int element)
+            {
+
+                for (var i = 0; i < this._proposedArray.Count; i++)
+                {
+                    if (this._proposedArray[i] == element) return i;
+                }
+                return -1;
+            }
+
+            public int binarySearch(int element)
+            {
+                int lowerbound = 0;
+                int upperBound = this._proposedArray.Count;
+                int midPoint = 0;
+
+            while (lowerbound <= upperBound) {
+
+                midPoint = (int)Math.Floor(Convert.ToDouble(lowerbound + upperBound) / 2);
+             
+                var midPointElement = this._proposedArray[midPoint];
+
+                if (midPointElement == element) return midPoint;
+
+                if (element < midPointElement) upperBound = midPoint - 1;
+
+                if (element > midPointElement) lowerbound = midPoint + 1;
+
+            }
+
+            return -1;
+            }
+
+    }
+}
